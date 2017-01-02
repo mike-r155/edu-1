cbcbc96f95008c0415f4102375f89ac7db82f6bb
mike-r155 <mike_everything@hotmail.com>
2017-01-03 03:45:51 +0900

    - Change autocrlf setting - Add button and textarea. - Add behavior that button pushing changes textarea message

--- app/src/main/java/edu/self/edu_1/MainActivity.java
+++ app/src/main/java/edu/self/edu_1/MainActivity.java
@@ -2,12 +2,23 @@ package edu.self.edu_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int clicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeLabel(View view) {
+        TextView text = (TextView) findViewById(R.id.myText);
+
+        this.clicked++;
+
+        text.setText("Pushed:" + String.valueOf(this.clicked));
     }
 }
